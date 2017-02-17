c491c7ba176ffa2ea2c74605c4a2ce0ff6d3946c
Nikita Kasatkin <nikita.kasatkin@exactprosystems.com>
2017-02-17 14:44:17 +0300

    17.02.2017 commit

---
+++ src/main/java/ru/mango/User.java
@@ -0,0 +1,65 @@
+package ru.mango;
+
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean
+@SessionScoped
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1094801825228386363L;
+
+    private String password;
+    private String message;
+    private String user;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String validateUsernamePassword() {
+        boolean valid = UserDAO.validate(user, password);
+        if (valid) {
+            HttpSession session = SessionUtils.getSession();
+            session.setAttribute("username", user);
+            return "admin";
+        } else {
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Incorrect Username and Passowrd",
+                            "Please enter correct username and Password"));
+            return "login";
+        }
+    }
+
+    public String logout() {
+        HttpSession session = SessionUtils.getSession();
+        session.invalidate();
+        return "login";
+    }
+}
